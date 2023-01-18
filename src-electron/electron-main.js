cbd69f8e675d169a0f08e3ab9ee72051cf56cef7
Safa HELVACI <95585412+kHealer@users.noreply.github.com>
2023-01-18 08:50:40 +0300

    setting width and height

--- src-electron/electron-main.js
+++ src-electron/electron-main.js
@@ -19,8 +19,8 @@ function createWindow () {
    */
   mainWindow = new BrowserWindow({
     icon: path.resolve(__dirname, 'icons/icon.png'), // tray icon
-    width: 1000,
-    height: 600,
+    width: 1024,
+    height: 720,
     useContentSize: true,
     webPreferences: {
       contextIsolation: true,
